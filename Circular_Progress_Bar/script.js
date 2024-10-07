7dfc4b3ee8f2027808a5febf76e5d55d83844b63
PrinceKumarCodes <princekumarcbcs2022@gmail.com>
2024-10-07 08:49:33 +0530

    Update files

--- Circular_Progress_Bar/script.js
+++ Circular_Progress_Bar/script.js
@@ -1,3 +1,4 @@
+// First percentage in circular progress bar 
 let number = document.getElementById("number");
 let counter = 0;
 setInterval(()=>{
@@ -7,4 +8,17 @@ setInterval(()=>{
     counter+=1;
     number.innerHTML = counter + "%";
     }
-},30)//30 miliseconds speed
+},30)//30 millisecond speed
+
+
+//Second percentage in circular progress bar
+let secondNumber = document.getElementById("number1");
+let secondCounter = 0;
+setInterval(()=>{
+    if(secondCounter == 80){
+        clearInterval();
+    }else{
+    secondCounter+=1;
+    secondNumber.innerHTML = secondCounter + "%";
+    }
+},25);
